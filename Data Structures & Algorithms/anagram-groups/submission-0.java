4a322e3e7b20f057d09291844833a75e68f059b5
Donovan Liu <144486880+donovanliu1@users.noreply.github.com>
2026-08-13 17:29:41 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> hashMap = new HashMap();
+        for (String str : strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedString = Arrays.toString(charArray);
+            if (hashMap.containsKey(sortedString)) hashMap.get(sortedString).add(str);
+            else hashMap.put(sortedString, new ArrayList<String>(Arrays.asList(str)));
+        }
+        List<List<String>> anagrams = new ArrayList(hashMap.values());
+        return anagrams;
+    }
+}
